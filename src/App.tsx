179813ac513b2a0825b35d543d20dbe13d7d8932
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-09-07 13:24:13 -0400

    show info user

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,20 @@ import { useSelector } from "react-redux";
 const App = () => {
   const user = useSelector((state: AppStore) => state.user);
   return (
-    <>
-      <span>{JSON.stringify(user)}</span>
+    <div>
+      <div className="flex flex-col gap-3 justify-center">
+        <div className="flex gap-2 items-center">
+          <h3>Name:</h3>
+          <p>{user?.name}</p>
+        </div>
+
+        <div className="flex gap-2 items-center">
+          <h3>Email:</h3>
+          <p>{user?.email}</p>
+        </div>
+      </div>
       <Home />
-    </>
+    </div>
   );
 };
 
